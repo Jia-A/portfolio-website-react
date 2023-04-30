d4044e67b56df88463015252f976c2aa9dd2bf11
Jia-A <agrawaljiya2011@gmail.com>
2023-05-01 02:43:55 +0530

    projects-array

---
+++ src/projectData.js
@@ -0,0 +1,38 @@
+import Store from "./assets/store.png"
+import Shoonya from "./assets/shoonya.png"
+import Notes from "./assets/notes.png"
+import Pomodoro from "./assets/pomodoro.png"
+import Techtonic from "./assets/techtonic.png"
+
+export const projects = [
+    {
+        title : "Dot Store",
+        desc : "An E-commerce wesbite",
+        src : Store,
+        link : ""
+    },
+    {
+        title : "Shoonya",
+        desc : "A video library",
+        src : Shoonya,
+        link : ""
+    },
+    {
+        title : "Dot notes",
+        desc : "A note taking website",
+        src : Notes,
+        link : ""
+    },
+    {
+        title : "Dot on Time",
+        desc : "A pomodoro app",
+        src : Pomodoro,
+        link : ""
+    },
+    {
+        title : "Techtonic UI",
+        desc : "A CSS component library",
+        src : Techtonic,
+        link : ""
+    }
+]
