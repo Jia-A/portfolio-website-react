2ddad898fad229ced3d893e6ef6525924b9f6afc
Jia-A <agrawaljiya2011@gmail.com>
2023-05-03 00:44:53 +0530

    navbar-name-changed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Blogs from './components/Blogs';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
-import { Navbar } from './components/Navbar';
+import { Navbar } from './components/navbar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 
