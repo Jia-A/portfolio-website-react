3147553178f96cab45ee523ab2b07d70a7fa6495
Jia-A <agrawaljiya2011@gmail.com>
2023-05-03 01:07:00 +0530

    minor-changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Footer() {
   return (
     <div className='text-center bg-[#F9F9F9] py-5 border-t border-[#1a1919]'>
-        <p>Made with ReactJS, TailwindCSS, Framer-motion</p>
-        <p>© | 2021 | jiyaagrawal</p>
+        <p className='text-[12px] md:text-[16px] inline border-x p-2 border-[#1a1919]'>Made with ReactJS - TailwindCSS - Framer-motion</p>
+        <p className='text-[12px] md:text-[15px]'>© | 2023 | jiyaagrawal</p>
     </div>
   )
 }
